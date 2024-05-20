aabb4ed58cb66538e6ed754b219aac77c307454b
Chan Lee <chanho0325@gmail.com>
2024-05-20 22:22:41 +0900

    feat: worker.go 스켈레톤 추가

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,32 @@
 package worker
+
+import (
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"orchestrator-from-scratch/task"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	// Collect stats
+	fmt.Println("Collecting stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("Starting or stopping a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("Starting a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("Stopping a task")
+}
