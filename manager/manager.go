90bd13cc31f4ee6d3b23617f746476dbca952771
Chan Lee <chanho0325@gmail.com>
2024-05-20 22:48:32 +0900

    feat: manager.go 스켈레톤 추가

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,29 @@
 package manager
+
+import (
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"orchestrator-from-scratch/task"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("Selecting a worker")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("Updating tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("Sending work to workers")
+}
