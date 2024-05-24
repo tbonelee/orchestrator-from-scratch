deae031dfde4d9fc935ff4530ee0c915964abdd6
Chan Lee <chanho0325@gmail.com>
2024-05-25 00:48:03 +0900

    feat: worker.StartTask(), worker.StopTask() 추가

--- worker/worker.go
+++ worker/worker.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"log"
 	"orchestrator-from-scratch/task"
+	"time"
 )
 
 type Worker struct {
@@ -23,10 +25,37 @@ func (w *Worker) RunTask() {
 	fmt.Println("Starting or stopping a task")
 }
 
-func (w *Worker) StartTask() {
-	fmt.Println("Starting a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult {
+	t.StartTime = time.Now().UTC()
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+	result := d.Run()
+	if result.Error != nil {
+		log.Printf("Error running task %v: %v\n", t.ID, result.Error)
+		t.State = task.Failed
+		w.Db[t.ID] = &t
+		return result
+	}
+
+	t.ContainerID = result.ContainerId
+	t.State = task.Running
+	w.Db[t.ID] = &t
+
+	return result
 }
 
-func (w *Worker) StopTask() {
-	fmt.Println("Stopping a task")
+func (w *Worker) StopTask(t task.Task) task.DockerResult {
+	config := task.NewConfig(&t)
+	d := task.NewDocker(config)
+
+	result := d.Stop(t.ContainerID)
+	if result.Error != nil {
+		log.Printf("Error stopping container %v: %v\n", t.ContainerID, result.Error)
+	}
+	t.FinishTime = time.Now().UTC()
+	t.State = task.Completed
+	w.Db[t.ID] = &t
+	log.Printf("Stopped and removed container %v for task %v\n", t.ContainerID, t.ID)
+
+	return result
 }
