545479cae5dc482bf0bc97437d6a335961bab809
Chan Lee <chanho0325@gmail.com>
2024-05-30 20:40:23 +0900

    feat: manager.New() 메소드 추가

--- manager/manager.go
+++ manager/manager.go
@@ -22,6 +22,25 @@ type Manager struct {
 	LastWorker    int
 }
 
+func New(workers []string) *Manager {
+	taskDb := make(map[uuid.UUID]*task.Task)
+	eventDb := make(map[uuid.UUID]*task.TaskEvent)
+	workerTaskMap := make(map[string][]uuid.UUID)
+	taskWorkerMap := make(map[uuid.UUID]string)
+	for _, w := range workers {
+		workerTaskMap[w] = []uuid.UUID{}
+	}
+
+	return &Manager{
+		Pending:       *queue.New(),
+		Workers:       workers,
+		TaskDb:        taskDb,
+		EventDb:       eventDb,
+		WorkerTaskMap: workerTaskMap,
+		TaskWorkerMap: taskWorkerMap,
+	}
+}
+
 func (m *Manager) AddTask(te task.TaskEvent) {
 	m.Pending.Enqueue(te)
 }
