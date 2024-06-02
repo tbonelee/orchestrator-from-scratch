bcc5a9610a9a3475afa30c267df130ef7a79c1a7
Chan Lee <chanho0325@gmail.com>
2024-06-02 14:30:39 +0900

    feat: RoundRobin 스케쥴러 구현 추가

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -10,3 +10,51 @@ type Scheduler interface {
 	Score(t task.Task, nodes []*node.Node) map[string]float64
 	Pick(scores map[string]float64, candidates []*node.Node) *node.Node
 }
+
+type RoundRobin struct {
+	Name       string
+	LastWorker int
+}
+
+func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
+	return nodes
+}
+func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
+	nodeScores := make(map[string]float64)
+	var newWorker int
+	if r.LastWorker+1 < len(nodes) {
+		newWorker = r.LastWorker + 1
+		r.LastWorker++
+	} else {
+		newWorker = 0
+		r.LastWorker = 0
+	}
+
+	for idx, node := range nodes {
+		if idx == newWorker {
+			nodeScores[node.Name] = 0.1 // Give the new worker the lowest score
+		} else {
+			nodeScores[node.Name] = 1.0 // Give all other workers a higher score
+		}
+	}
+
+	return nodeScores
+}
+func (r *RoundRobin) Pick(scores map[string]float64, candidates []*node.Node) *node.Node {
+	var bestNode *node.Node
+	var lowestScore float64
+	for idx, node := range candidates {
+		if idx == 0 {
+			bestNode = node
+			lowestScore = scores[node.Name]
+			continue
+		}
+
+		if scores[node.Name] < lowestScore {
+			bestNode = node
+			lowestScore = scores[node.Name]
+		}
+	}
+
+	return bestNode
+}
